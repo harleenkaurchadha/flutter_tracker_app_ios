e5b7772bf553f40c259e7a7ed4cde596084e20ce
Harleen Kaur <harleen198.chadha@gmail.com>
2020-06-06 20:22:43 +0530

    adjusted modal sheet form for keyboard

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -55,54 +55,56 @@ class _NewTransactionState extends State<NewTransaction> {
   @override
 
   Widget build(BuildContext context){
-    return Card(child: Container(
-      padding: EdgeInsets.only(
-        left: 10,
-        right: 10,
-        top: 10,
-  //      bottom: MediaQuery.of(context).viewInsets.bottom + 10,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children:[
-          TextField(
-            decoration: InputDecoration(labelText: 'Title'),
-            controller: _titleController,                                     //controller listens and saves the user input
-            onSubmitted: (_) => _submitData(),
-            ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Amount'),
-            controller: _amountController,
-            keyboardType: TextInputType.number,                        //number keyboard should be displayed
-            onSubmitted: (_) => _submitData(),                       //just the syntax
-          ),
-          Container(
-            height: 70,
-            child: Row(children: [
-             Expanded(
-              child: Text((_selectedDate== null) ? 'No Date Chosen!' : 'Picked Date: ${DateFormat.yMMMd().format(_selectedDate)}'),
-             ),
-            FlatButton(
-              child:
-              Text('Choose Date',
-              style: TextStyle(
-              fontWeight: FontWeight.bold)
+    return SingleChildScrollView(
+      child: Card(child: Container(
+        padding: EdgeInsets.only(
+          left: 10,
+          right: 10,
+          top: 10,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 15,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children:[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: _titleController,                                     //controller listens and saves the user input
+              onSubmitted: (_) => _submitData(),
               ),
-              textColor: Theme.of(context).primaryColor,
-              onPressed: _presentDatePicker,
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: _amountController,
+              keyboardType: TextInputType.number,                        //number keyboard should be displayed
+              onSubmitted: (_) => _submitData(),                       //just the syntax
             ),
-          ],),
-          ),
-          RaisedButton(
-            child: Text('Add Transaction'),
-            color: Theme.of(context).primaryColor,
-            textColor: Theme.of(context).textTheme.button.color,
-            onPressed: _submitData,
-          ),
-        ],
+            Container(
+              height: 70,
+              child: Row(children: [
+               Expanded(
+                child: Text((_selectedDate== null) ? 'No Date Chosen!' : 'Picked Date: ${DateFormat.yMMMd().format(_selectedDate)}'),
+               ),
+              FlatButton(
+                child:
+                Text('Choose Date',
+                style: TextStyle(
+                fontWeight: FontWeight.bold)
+                ),
+                textColor: Theme.of(context).primaryColor,
+                onPressed: _presentDatePicker,
+              ),
+            ],),
+            ),
+            RaisedButton(
+              child: Text('Add Transaction'),
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).textTheme.button.color,
+              onPressed: _submitData,
+            ),
+          ],
+        ),
       ),
-    ),
-      elevation: 5,);
+        elevation: 5,),
+    );
 
   }
 }
